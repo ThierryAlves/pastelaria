0391438668edb490d5d12e8dbf35735e0d14cbf8
Thierry Alves <thierryalves.oliveira21@gmail.com>
2023-10-22 11:55:29 -0300

    Adicionando seeder de produtos

--- pastelaria/database/seeders/ProdutosSeeder.php
+++ pastelaria/database/seeders/ProdutosSeeder.php
@@ -16,8 +16,36 @@ class ProdutosSeeder extends Seeder
         $insert = [
             [
                 'nome' => 'Pastel de carne',
+                'preco' => 5,
+                'foto_produto' => 'phpQy6vue.jpg',
+                'created_at' => Carbon::now()->subDays(rand(1, 30)),
+                'updated_at' => Carbon::now()->addDays(rand(1, 30)),
+            ],
+            [
+                'nome' => 'Pastel de frango',
+                'preco' => 5,
+                'foto_produto' => 'phpdACMwc.jpg',
+                'created_at' => Carbon::now()->subDays(rand(1, 30)),
+                'updated_at' => Carbon::now()->addDays(rand(1, 30)),
+            ],
+            [
+                'nome' => 'Pastel de bauru',
                 'preco' => 6.50,
-                'path_foto' => 'pastel.jpg',
+                'foto_produto' => 'phpVPJEQb.jpg',
+                'created_at' => Carbon::now()->subDays(rand(1, 30)),
+                'updated_at' => Carbon::now()->addDays(rand(1, 30)),
+            ],
+            [
+                'nome' => 'Pastel de pizza',
+                'preco' => 6.50,
+                'foto_produto' => 'phpnsK2Pd.jpg',
+                'created_at' => Carbon::now()->subDays(rand(1, 30)),
+                'updated_at' => Carbon::now()->addDays(rand(1, 30)),
+            ],
+            [
+                'nome' => 'Pastel de carne seca',
+                'preco' => 8.50,
+                'foto_produto' => 'phpj8T5oj.jpg',
                 'created_at' => Carbon::now()->subDays(rand(1, 30)),
                 'updated_at' => Carbon::now()->addDays(rand(1, 30)),
             ],
